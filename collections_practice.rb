d0b558d31bf466b71e8d61b5e4842663224241ac
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-30 17:43:49 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -41,7 +41,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  [0, 2, 3].each_with_index.collect do |element, index|
+  array[0, 2, 3].each_with_index.collect do |element, index|
     element[-1] = "s" 
   end
 end 
