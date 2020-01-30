34fcf1e2bc5191b9718af741ee2f22e31a317fb4
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-01-30 17:28:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(numbers)
+  numbers.sort 
+end
 
+def sort_array_desc(numbers)
+  numbers.sort.reverse
+end 
+
+def sort_array_char_count(numbers)
+  numbers.sort_by(&:length)
+end 
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+  new_array = []
+   array.each do |string|
+     new_array << 
+end 
+
+def find_a
+end 
+
+def sum_array
+end 
+
+def add_s
+end 
